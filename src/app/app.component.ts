3be198f13555055bd1eaa440acebdbe670b8ac56
Bammmmmm <155812@ettyhillesumlyceum.nl>
2021-11-19 16:47:49 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  a: string;
+  b: string;
+
+bamm(){
+
+}
+
   name = 'Angular ' + VERSION.major;
 }
