60bac7ee721a5040a28637761a05de835cd232be
Bammmmmm <155812@ettyhillesumlyceum.nl>
2021-12-03 16:52:39 +0100

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Bammm1Component } from './bammm1/bammm1.component';
 import { Bammm2Component } from './bammm2/bammm2.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
   declarations: [ AppComponent, HelloComponent, Bammm1Component, Bammm2Component ],
   bootstrap:    [ AppComponent ]
 })
